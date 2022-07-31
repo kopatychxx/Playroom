7cf7443a78829789835b94b01d2d2e7d7191db97
kopatychxx <evgenijsalo88@gmail.com>
2022-07-31 14:28:41 +0200

    Create project and add Header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main>
+    <router-link to="/"</router-link>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style scoped>
+
+</style>
